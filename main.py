6d4690c70cca54ac265d7f04656d71ccd901b180
uxdata academy <112259627+uxdata-academy@users.noreply.github.com>
2024-05-07 23:03:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-import requests
-import json
-import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
-from google.cloud import bigquery
-import os
-from pandas.io import gbq
-import pandas_gbq
+import functions_framework
 
-def hello_http():
-  print("Hello")
-  return("Hello")
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(name)
